58e67253e34e170a7d47aca59e25180987f93080
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-04-07 23:47:37 +0200

    added Icons calendar and level

--- SoftUni-Exam-Project-Gym/client/src/components/CatalogItem/CatalogItem.js
+++ SoftUni-Exam-Project-Gym/client/src/components/CatalogItem/CatalogItem.js
@@ -1,21 +1,24 @@
 import catalogItemCSS from '../../imported-elements/css/catalogItem.module.css';
 import { Link } from 'react-router-dom';
+import CalendarIcon from '../../imported-elements/images/calendarIcon.png'
+import LevelIcon from '../../imported-elements/images/level.png'
+
+
 export const CatalogItem = ({ plan }) => {
 
     return (
         <div className={catalogItemCSS["card"]}>
-            <h3>{plan.day}</h3>
             <div className={catalogItemCSS["info"]}>
-                <div className={catalogItemCSS["icon-time"]}>
-
+                <div>
+                    <img src={LevelIcon} alt="Calendar icon" />
                 </div>
-                <h4>{plan.time}</h4>
+                <h4>{plan.level}</h4>
             </div>
             <div className={catalogItemCSS["info"]}>
-                <div className={catalogItemCSS["icon-group"]}>
-
+                <div>
+                    <img src={CalendarIcon} alt="Calendar icon" />
                 </div>
-                <h4>{plan.muscleGroup}</h4>
+                <h4>{plan.days} times per week</h4>
             </div>
 
             <Link to={`/plans/${plan._id}/details`} className="details-button">
