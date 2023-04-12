b3598d99d1b594283d6840fee2468d9dc209a646
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-04-13 00:48:02 +0200

    added stylin for PageNotFound

--- SoftUni-Exam-Project-Gym/client/src/components/PageNotFound.js
+++ SoftUni-Exam-Project-Gym/client/src/components/PageNotFound.js
@@ -1,15 +1,10 @@
+import pageNotFoundCSS from '../imported-elements/css/pageNotFound.module.css';
+
 export const PageNotFound = () => {
 
     //TODO: Edit this page if the user search not existing page
     return (
-        <div>
-            <br></br>
-            <br></br>
-
-            <br></br>
-
-            <br></br>
-
+        <div className={pageNotFoundCSS["div"]}>
             <h1> PAGE NOT FOUND </h1>
         </div>
     );
