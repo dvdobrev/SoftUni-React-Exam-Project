87a6ef00b7ff4f409c9b458263259306197d08a9
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-04-15 00:01:07 +0200

    changed the text on the not found page

--- SoftUni-Exam-Project-Gym/app/src/components/PageNotFound.js
+++ SoftUni-Exam-Project-Gym/app/src/components/PageNotFound.js
@@ -4,7 +4,7 @@ export const PageNotFound = () => {
 
     return (
         <div className={pageNotFoundCSS["div"]}>
-            <h1> PAGE NOT FOUND </h1>
+            <h1> THERE IS AN ERROR OR THE PAGE IS NOT FOUND </h1>
         </div>
     );
 };
