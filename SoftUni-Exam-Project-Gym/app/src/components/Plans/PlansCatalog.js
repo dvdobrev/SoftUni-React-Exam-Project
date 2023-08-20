f2e7d6fd1104855bbfb7088c34f5dc9edc68eca9
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-08-21 00:24:41 +0200

    used useMemo

--- SoftUni-Exam-Project-Gym/app/src/components/Plans/PlansCatalog.js
+++ SoftUni-Exam-Project-Gym/app/src/components/Plans/PlansCatalog.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 
 import { CatalogItem } from "../CatalogItem/CatalogItem";
 import catalogCSS from '../../imported-elements/css/catalog.module.css';
@@ -10,6 +10,9 @@ export const PlansCatalog =  () => {
 
     const { trainingPlans } = useContext(PlanContext);
 
+    const memoizedTrainingPlans = useMemo(() => trainingPlans, [trainingPlans]);
+
+
     return (
 
         <section id={`${catalogCSS["catalog-page"]}`}>
@@ -17,8 +20,8 @@ export const PlansCatalog =  () => {
             <h1>All Plans</h1>
 
             <div className={catalogCSS["card-container"]}>
-                {trainingPlans.length > 0 ? (
-                    trainingPlans.map(plan => <CatalogItem key={plan.planId} plan={plan} />)
+                {memoizedTrainingPlans.length > 0 ? (
+                    memoizedTrainingPlans.map(plan => <CatalogItem key={plan.planId} plan={plan} />)
                 ) : (
                     <h3 className="no-articles">No plans yet</h3>
                 )}
