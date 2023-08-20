112308485d37bc248959ba26f3bab3e6e7682f79
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-08-21 00:28:01 +0200

    code refactoring

--- SoftUni-Exam-Project-Gym/app/src/components/Logout.js
+++ SoftUni-Exam-Project-Gym/app/src/components/Logout.js
@@ -5,22 +5,13 @@ import { UserContext } from '../contexts/UserContext';
 
 export const Logout = () => {
     const navigate = useNavigate();
-    const { userData, logoutHandler } = useContext(UserContext);
+    const { logoutHandler } = useContext(UserContext);
 
 
     useEffect(() => {
         logoutHandler()
         // localStorage.clear()
         navigate('/')
-            // userServices.logout(userData.accessToken)
-            //     .then(() => {
-            //         logoutHandler();
-            //         localStorage.clear();
-            //         navigate('/');
-            //     })
-            // .catch(() => {
-            //     navigate('/');
-            // });
     });
 
     return null;
