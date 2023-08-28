1cc77ca0313f946ed5a1e041e46fbec3720502c7
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-08-29 00:15:03 +0200

    added the video logik for the main page again, but set it in a comment

--- SoftUni-Exam-Project-Gym/app/src/components/MainBanner.js
+++ SoftUni-Exam-Project-Gym/app/src/components/MainBanner.js
@@ -12,13 +12,33 @@ import { NavLink } from 'react-router-dom'
 export const MainBanner = () => {
     const { userData } = useContext(UserContext);
 
+    // return (
+    //     <div className={mainBannerCSS["main-banner"]} id="top">
+    //         <video autoPlay muted loop id={mainBannerCSS["bg-video"]}>
+    //             <source src={video} type="video/mp4" />
+    //         </video>
+
+    //         <div className={`${mainBannerCSS["video-overlay"]} ${styles["header-text"]}}`}>
+    //             <div className={mainBannerCSS["caption"]}>
+    //                 <h6>work harder, get stronger</h6>
+    //                 <h2>easy with our <em>gym</em></h2>
+    //                 {!userData?.email &&
+    //                     <div>
+    //                         <NavLink className={styles["buttons"]} to="/register">Become a member</NavLink>
+    //                     </div>
+    //                 }
+    //             </div>
+    //         </div>
+    //     </div>
+    // )
+
     return (
         <div className={mainBannerCSS["main-banner"]} id="top">
-            <video autoPlay muted loop id={mainBannerCSS["bg-video"]}>
-                <source src={video} type="video/mp4" />
-            </video>
+            <div id={mainBannerCSS["bg-img"]}>
+                {/* <source src={video} type="video/mp4" /> */}
+            </div>
 
-            <div className={`${mainBannerCSS["video-overlay"]} ${styles["header-text"]}}`}>
+            <div className={`${styles["header-text"]}}`}>
                 <div className={mainBannerCSS["caption"]}>
                     <h6>work harder, get stronger</h6>
                     <h2>easy with our <em>gym</em></h2>
