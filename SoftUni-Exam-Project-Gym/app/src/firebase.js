eacb599c59dbcb3832a033cbc16b49f1c831c671
Dobrin Dobrev <dobrin.valev.dobrev@gmail.com>
2023-08-30 22:46:49 +0200

    deleted todos

--- SoftUni-Exam-Project-Gym/app/src/firebase.js
+++ SoftUni-Exam-Project-Gym/app/src/firebase.js
@@ -2,12 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-
-
-//TODO: make the apiKey in .env - now it comes
-// with error thast why i use the key directly
-
-
 // web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_KEY,
